a7a94bde83b4b457890c2fef85950443c7d76a15
Rodrigo Muniz <rtmuniz@positivo.com.br>
2019-08-28 15:51:37 -0300

    Some changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './App'
-import './index.css'
-import registerServiceWorker from './registerServiceWorker'
-import { store } from './store'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import { store } from "./store";
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>
-    , document.getElementById('root'))
-registerServiceWorker()
+    , document.getElementById("root"));
+registerServiceWorker();
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,19 @@
-import * as localforage from 'localforage'
+import * as localforage from "localforage";
 
 export const loadState = () => {
-    try {
-        return localforage.getItem('onbState').then(state => state || {})
-    }
-    catch(ex) {
-        Promise.resolve({})
-    }
+  try {
+    return localforage.getItem("onbState").then((state) => state || {});
+  } catch (ex) {
+    Promise.resolve({});
+  }
 
-    return null
-}
+  return null;
+};
 
 export const saveState = (state: any) => {
   try {
-    localforage.setItem('localState', state)
+    localforage.setItem("localState", state);
   } catch (ex) {
-    window.console.warn('cannot save state with localforage', ex)
+    window.console.warn("cannot save state with localforage", ex);
   }
-}
+};
