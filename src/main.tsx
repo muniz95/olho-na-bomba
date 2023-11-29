3f0974ab0104725070fb23e63c56cd9cadbc77f7
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2023-11-29 07:58:54 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { store } from './store'
+import { store } from './store/index.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
